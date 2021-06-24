5b6e2425180d073e566a618eb7863765b3fe8bc8
Mihir Chauhan <39073758+mihir-chauhan@users.noreply.github.com>
2021-06-23 18:33:59 -0700

    Working Group Creation, Working on populating groups - then add going into groups

--- innovation_challenge/lib/createGroupPage.dart
+++ innovation_challenge/lib/createGroupPage.dart
@@ -1,16 +1,49 @@
+import 'package:DigiHealth/provider_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_form_bloc/flutter_form_bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:DigiHealth/appPrefs.dart';
+import 'package:horizontal_card_pager/card_item.dart';
+import 'package:horizontal_card_pager/horizontal_card_pager.dart';
 
 class CreateGroupPage extends StatefulWidget {
   @override
   _CreateGroupPageState createState() => _CreateGroupPageState();
 }
 
+List<CardItem> groupImageChoices = [
+  ImageCarditem(
+      image: Image.network(
+          "https://firebasestorage.googleapis.com/v0/b/innov8rz-innovation-challenge.appspot.com/o/DigiGroup_1.png?alt=media&token=d7dbb7ef-ac75-4eeb-a732-cf3bf9c207e7"))
+];
+List<String> groupImageChoiceLinks = [];
+int selectedImageIndex = -1;
+User user;
+
 class _CreateGroupPageState extends State<CreateGroupPage> {
+  bool hasUpdatedGroupImageChoices = false;
   @override
   Widget build(BuildContext context) {
+    user = Provider.of(context).auth.firebaseAuth.currentUser;
+    if (!hasUpdatedGroupImageChoices) {
+      hasUpdatedGroupImageChoices = true;
+      groupImageChoices.clear();
+      FirebaseFirestore.instance
+          .collection("Global Data")
+          .doc("DigiGroup Images")
+          .get()
+          .then((DocumentSnapshot doc) {
+        Map<String, dynamic> map = doc.data();
+        map.forEach((key, value) {
+          setState(() {
+            groupImageChoiceLinks.add(value.toString());
+            groupImageChoices.add(ImageCarditem(image: Image.network(value)));
+          });
+        });
+      });
+    }
     return CupertinoPageScaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: secondaryColor,
@@ -34,7 +67,6 @@ class _CreateGroupPageState extends State<CreateGroupPage> {
           body: AllFieldsForm(),
         ));
   }
-
 }
 
 class AllFieldsFormBloc extends FormBloc<String, String> {
@@ -53,20 +85,55 @@ class AllFieldsFormBloc extends FormBloc<String, String> {
 
   @override
   void onSubmitting() async {
-    if(groupName.value.length < 2 || groupName.value.length > 15 ) {
+    if (groupName.value.length < 2 || groupName.value.length > 15) {
       groupName.addFieldError('Must be between 2 and 15 characters');
     }
-    if(groupVisibility.value == null) {
+    if (groupVisibility.value == null) {
       groupVisibility.addFieldError('Please select an option');
     }
-    if(groupName.value.length > 2 && groupName.value.length < 15 && groupVisibility.value != null) {
+    if (groupName.value.length > 2 &&
+        groupName.value.length < 15 &&
+        groupVisibility.value != null) {
       //Create group in Firebase DB
+      FirebaseFirestore.instance
+          .collection("DigiGroup")
+          .doc(groupName.value)
+          .set({
+        "visibility": groupVisibility.value.toString(),
+        "image": groupImageChoiceLinks.elementAt(selectedImageIndex)
+      }, SetOptions(merge: true));
+
+      String text =  groupVisibility.value == "Public" ? " to all users." : ".";
+      FirebaseFirestore.instance
+          .collection("DigiGroup")
+          .doc(groupName.value)
+          .collection("Chat")
+          .add({
+        "created": DateTime.now(),
+        "message": "Welcome to your personal group! Feel free to chat and encourage other group members. Your group is ${groupVisibility.value.toString().toLowerCase()}" + text + " Have fun! - DigiHealth Team",
+        "sentBy": "DigiHealth",
+      });
+      FirebaseFirestore.instance
+          .collection("DigiGroup")
+          .doc(groupName.value)
+          .collection("Members")
+          .doc(user.email)
+          .set({
+        "Name": user.email,
+        "Points": 0,
+        "isAdmin": true,
+      });
       emitSuccess();
     }
   }
 }
 
-class AllFieldsForm extends StatelessWidget {
+class AllFieldsForm extends StatefulWidget {
+  @override
+  _AllFieldsFormState createState() => _AllFieldsFormState();
+}
+
+class _AllFieldsFormState extends State<AllFieldsForm> {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
@@ -84,134 +151,91 @@ class AllFieldsForm extends StatelessWidget {
               ),
             ),
             child: Scaffold(
-              backgroundColor: primaryColor,
-              floatingActionButton: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  FloatingActionButton.extended(
-                    backgroundColor: tertiaryColor,
-                    heroTag: "CreateGroupButton",
-                    onPressed: formBloc.submit,
-                    icon: Icon(Icons.add_rounded),
-                    label: Text('Create Group', style: TextStyle(fontFamily: 'Nunito'),),
-                  ),
-                ],
-              ),
-              body: FormBlocListener<AllFieldsFormBloc, String, String>(
-                onSuccess: (context, state) {
-                  Navigator.of(context).pop();
-                },
-                child: SingleChildScrollView(
-                  physics: ClampingScrollPhysics(),
-                  child: Padding(
-                    padding: const EdgeInsets.all(24.0),
-                    child: Column(
-                      children: <Widget>[
-                        TextFieldBlocBuilder(
-                          textFieldBloc: formBloc.groupName,
-                          decoration: InputDecoration(
-                            labelText: 'Group Name',
-                            prefixIcon: Icon(Icons.drive_file_rename_outline, color: tertiaryColor,),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                              borderSide:
-                                  BorderSide(color: secondaryColor, width: 1.0),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                              borderSide:
-                                  BorderSide(color: Colors.white, width: 1.0),
-                            ),
-                            labelStyle: TextStyle(fontFamily: 'Nunito', color: tertiaryColor),
-                            errorStyle: TextStyle(fontFamily: 'Nunito'),
-                          ),
-                          style: TextStyle(fontFamily: 'Nunito', color: Colors.white),
-                          cursorColor: Colors.white,
-                        ),
-                        RadioButtonGroupFieldBlocBuilder<String>(
-                          selectFieldBloc: formBloc.groupVisibility,
-                          decoration: InputDecoration(
-                            labelText: 'Group Visibility',
-                            prefixIcon: SizedBox(),
-                            labelStyle: TextStyle(fontFamily: 'Nunito'),
-                            errorStyle: TextStyle(fontFamily: 'Nunito'),
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                              borderSide:
-                              BorderSide(color: secondaryColor, width: 1.0),
-                            ),
-                          ),
-                          itemBuilder: (context, item) => item,
-                        ),
-                      ],
+                backgroundColor: primaryColor,
+                floatingActionButton: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    FloatingActionButton.extended(
+                      backgroundColor: tertiaryColor,
+                      heroTag: "CreateGroupButton",
+                      onPressed: formBloc.submit,
+                      icon: Icon(Icons.check),
+                      label: Text(
+                        'Create Group',
+                        style: TextStyle(fontFamily: 'Nunito'),
+                      ),
                     ),
-                  ),
+                  ],
                 ),
-              ),
-            ),
+                body: FormBlocListener<AllFieldsFormBloc, String, String>(
+                  onSuccess: (context, state) {
+                    Navigator.of(context).pop();
+                  },
+                  child: SingleChildScrollView(
+                      physics: ClampingScrollPhysics(),
+                      child: Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: Column(
+                          children: <Widget>[
+                            TextFieldBlocBuilder(
+                              textFieldBloc: formBloc.groupName,
+                              decoration: InputDecoration(
+                                labelText: 'Group Name',
+                                prefixIcon: Icon(
+                                  Icons.drive_file_rename_outline,
+                                  color: tertiaryColor,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: BorderSide(
+                                      color: secondaryColor, width: 1.0),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: BorderSide(
+                                      color: Colors.white, width: 1.0),
+                                ),
+                                labelStyle: TextStyle(
+                                    fontFamily: 'Nunito', color: tertiaryColor),
+                                errorStyle: TextStyle(fontFamily: 'Nunito'),
+                              ),
+                              style: TextStyle(
+                                  fontFamily: 'Nunito', color: Colors.white),
+                              cursorColor: Colors.white,
+                            ),
+                            RadioButtonGroupFieldBlocBuilder<String>(
+                              selectFieldBloc: formBloc.groupVisibility,
+                              decoration: InputDecoration(
+                                labelText: 'Group Visibility',
+                                prefixIcon: SizedBox(),
+                                labelStyle: TextStyle(fontFamily: 'Nunito'),
+                                errorStyle: TextStyle(fontFamily: 'Nunito'),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                  borderSide: BorderSide(
+                                      color: secondaryColor, width: 1.0),
+                                ),
+                              ),
+                              itemBuilder: (context, item) => item,
+                            ),
+                            SizedBox(height: 10),
+                            Text("Select a group icon:", style: TextStyle(fontFamily: 'Nunito', fontSize: 15, color: tertiaryColor)),
+                            SizedBox(height: 10),
+                            HorizontalCardPager(
+                              initialPage: 0, // default value is 2
+                              onPageChanged: (page) {
+                                selectedImageIndex = page.round();
+                              },
+                              onSelectedItem: (page) =>
+                                  print("selected : $page"),
+                              items: groupImageChoices,
+                            )
+                          ],
+                        ),
+                      )),
+                )),
           );
         },
-      ),
-    );
-  }
-}
-
-class LoadingDialog extends StatelessWidget {
-  static void show(BuildContext context, {Key key}) => showDialog<void>(
-    context: context,
-    useRootNavigator: false,
-    barrierDismissible: false,
-    builder: (_) => LoadingDialog(key: key),
-  ).then((_) => FocusScope.of(context).requestFocus(FocusNode()));
-
-  static void hide(BuildContext context) => Navigator.pop(context);
-
-  LoadingDialog({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async => false,
-      child: Center(
-        child: Card(
-          child: Container(
-            width: 80,
-            height: 80,
-            padding: EdgeInsets.all(12.0),
-            child: CircularProgressIndicator(),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class SuccessScreen extends StatelessWidget {
-  SuccessScreen({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(Icons.tag_faces, size: 100),
-            SizedBox(height: 10),
-            Text(
-              'Success',
-              style: TextStyle(fontSize: 54, color: Colors.black),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 10),
-            ElevatedButton.icon(
-              onPressed: () => Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (_) => AllFieldsForm())),
-              icon: Icon(Icons.replay),
-              label: Text('AGAIN'),
-            ),
-          ],
-        ),
       ),
     );
   }
